37c1cf42b9628d0571ba39381466b7d82267cc71
medic <medic@DESKTOP-CJ7HL8C>
2022-01-28 21:43:08 -0500

    Add project files.

---
+++ Tech Assignment 2 - Basic Console App/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Robyn Lyn Medicielo
+
+        Console.WriteLine("\"I suppose in the end, the whole of life becomes an act of " +
+        "letting go, but what always hurts the most is not taking a moment to say goodbye\", " +
+        "is my favorite quote from Life of Pi. This is Robyn Lyn Medicielo's submission.");
+    }
+
+}
